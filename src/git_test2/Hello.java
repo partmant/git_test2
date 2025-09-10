3ae5bead0598ac4a26a6768e4579873b8488f8f7
partmant <ghdckdgml191@gmail.com>
2025-09-10 17:10:54 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,18 +2,10 @@ package git_test2;
 
 public class Hello {
 
-	String message;
+	String message = "안녕하세요";
 
-	public Hello() {
-		this("안녕하세요");
-	}
-
-	public Hello(String message) {
-		this.message = message;
-	}
-
-	public void say() {
-		System.out.println(message);
+	public String say() {
+		return message;
 	}
 
 }
